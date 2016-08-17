f23d32b965c78ec4418362adf5c1e1ef95d8adea
Alexei <alexeitsb@gmail.com>
2016-08-17 09:44:27 -0300

    Better comments

--- apod.rb
+++ apod.rb
@@ -4,29 +4,30 @@ require "net/http"
 require "open-uri"
 require "json"
 
-# Set nasa apod api url.
+# Set nasa apod api url
 nasa_apod_url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 
-# Set folder to download apod file.
+# Set folder to save apod file
 download_folder = "/home/alexei/apodfiles"
 
-# Request nasa apod API.
+# Request nasa apod API
 system("echo requesting api")
 response = Net::HTTP.get(URI(nasa_apod_url))
 system("echo got response")
 
 media_type = JSON.parse(response)["media_type"]
+
 system("echo media type is #{media_type}")
 
-# Check if media type is an image (may be a video or other).
+# Check if media type is an image (may be a video or something else)
 if media_type == "image"  
-  # Parse response and get url of apod file.
+  # Parse response and get url of apod file
   apod_url = JSON.parse(response)["url"]
 
-  # Find name of the file.
+  # Find the file name
   file_name = apod_url[apod_url.rindex("/")+1..apod_url.length]  
   
-  # Write file to download folder and set image as gnome wallpaper.  
+  # Write file and set image as gnome wallpaper
   File.open("#{download_folder}/#{file_name}", "wb") do |file|
     file.write open(apod_url).read
     system("echo file #{file_name} written to #{download_folder}")
