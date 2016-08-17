5fef253dd9d2fcd950d5bf94677a6bdce50b0550
Alexei <alexeitsb@gmail.com>
2016-08-17 10:00:55 -0300

    Printing some info

--- apod.rb
+++ apod.rb
@@ -15,21 +15,32 @@ system("echo requesting api")
 response = Net::HTTP.get(URI(nasa_apod_url))
 system("echo got response")
 
+# Parse the response
+date = JSON.parse(response)["date"]
+explanation = JSON.parse(response)["explanation"]
 media_type = JSON.parse(response)["media_type"]
+title = JSON.parse(response)["title"]
+url = JSON.parse(response)["url"]
 
 system("echo media type is #{media_type}")
 
 # Check if media type is an image (may be a video or something else)
-if media_type == "image"  
-  # Parse response and get url of apod file
-  apod_url = JSON.parse(response)["url"]
+if media_type == "image"
+  # Get the file name
+  file_name = url[url.rindex("/")+1..url.length]  
 
-  # Find the file name
-  file_name = apod_url[apod_url.rindex("/")+1..apod_url.length]  
+  # Print some info
+  system("echo")
+  system("echo #{date}")
+  system("echo")
+  system("echo #{title}")
+  system("echo")
+  system("echo #{explanation}")
+  system("echo")
   
   # Write file and set image as gnome wallpaper
   File.open("#{download_folder}/#{file_name}", "wb") do |file|
-    file.write open(apod_url).read
+    file.write open(url).read
     system("echo file #{file_name} written to #{download_folder}")
     system("echo setting image as wallpaper")
     system("gsettings set org.gnome.desktop.background picture-uri file://#{download_folder}/#{file_name}")
